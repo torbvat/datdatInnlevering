bf981b2bf23afe8329552be533a246a3510d18e9
tobiasfremming <tobias.fremming@gmail.com>
2022-03-22 16:01:48 +0100

    mekket grensesnitt. kommentert ut under interface

--- main/index.py
+++ main/index.py
@@ -111,18 +111,7 @@ def newUser():
             connection.close()
 
 
-# print("ny bruker? Y/N")
-# userInput = input()
-# if userInput == "Y":
-#     newUser()
-# elif userInput == "N":
-#     data = login()
-#     if data != None:
-#         session = True
-#         user = User(data[0][2], data[0][1], data[0][0])
 
-# if session:
-#     pass
 
 #Brukerhistorie 2:
 # def smakt_flest_unike_kaffer(): #Sjekk om dette funker etter at vi har lagt inn data i tabellene
@@ -203,3 +192,84 @@ def mest_for_pengene():
 # ikke_vaskede_fra_Rwanda_eller_Colombia() #skal få [kaffenavn3,brennerinavn3]    
 
 mest_for_pengene()
+
+#=======================================================================
+# user interface
+
+# while True:
+#     print("ny bruker? Y/N")
+#     userInput = input()
+#     if userInput == "Y":
+#         newUser()
+#         continue
+#     elif userInput == "N":
+#         data = login()
+#         if data != None:
+#             session = True
+#             user = User(data[0][2], data[0][1], data[0][0])
+
+    
+#     while True:
+#         if session == False:
+#             break
+#         print("Velg hva du vil gjøre:")
+#         print("(skriv tallet til alternativet du velger)")
+#         print("1: vurder en kaffe")
+#         print("2: sett inn informasjon i en tabell")
+#         print("3: test en brukerhistorie")
+#         print("4: logg ut")
+
+#         userInput = input()
+
+#         if userInput == "1":
+#             command.newKaffeSmaking(user.mail)
+#             continue
+
+#         if userInput == "2":
+#             while True:
+#                 print("velg tabell")
+#                 print("(skriv tallet til alternativet du velger)")
+#                 print("1: Foredlingsmetode")
+#                 print("2: Regioner")
+#                 print("3: Kaffegaard")
+#                 print("4: KaffeParti")
+#                 print("5: FerdigbrentKaffe")
+#                 print("6: KaffeBonne")
+#                 print("7: BestaarAv")
+#                 print("8: DyrketAv")
+#                 print("9: gå tilbake")
+            
+#                 tableInput = input()
+#                 if tableInput == "1": table = "Foredlingsmetode"
+#                 elif tableInput == "2": table = "Regioner"
+#                 elif tableInput == "3": table = "Kaffegaard"
+#                 elif tableInput == "4": table = "KaffeParti"
+#                 elif tableInput == "5": table = "FerdigbrentKaffe"
+#                 elif tableInput == "6": table = "KaffeBonne"
+#                 elif tableInput == "7": table = "BestaarAv"
+#                 elif tableInput == "8": table = "DyrketAv"
+#                 elif tableInput == "9": break
+#                 else: 
+#                     print("noe gikk galt, prøv igjen") 
+#                     continue
+#                 command.insert(table)
+#                 break
+        
+#         elif userInput == "3":
+#             print("velg brukerhistorie, ikke satt inn enda")
+#             continue
+
+#         elif userInput == "4":
+#             session = False
+#             continue
+
+#         else:
+#             print("something went wrong")
+#             continue
+
+        
+
+
+
+
+
